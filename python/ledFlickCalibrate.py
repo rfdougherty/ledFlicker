862293520b0ea85493d9a6bf307aea07b2ea6d5d
Bob Dougherty <bobd@stanford.edu>
2010-04-07 06:28:01 +0000

    almost there

--- python/ledFlickCalibrate.py
+++ python/ledFlickCalibrate.py
@@ -30,41 +30,85 @@ for i in range(nRepeats):
     ledSer.write('[m,0,0,0,0,0,0]\n');
     time.sleep(0.1)
     pr650.measure()
-    lum[i] = pr650.getLum()
+    lum[i,0] = pr650.getLum()
     [nm,s] = pr650.getSpectrum()
     spec[i,0,:] = s
     # measure red spectrum
     ledSer.write('[m,1,0,0,1,0,0]\n');
     time.sleep(0.1)
     pr650.measure()
-    lum[i] = pr650.getLum()
+    lum[i,1] = pr650.getLum()
     [nm,s] = pr650.getSpectrum()
     spec[i,1,:] = s
     # measure green spectrum
     ledSer.write('[m,0,1,0,0,1,0]\n');
     time.sleep(0.1)
     pr650.measure()
-    lum[i] = pr650.getLum()
+    lum[i,2] = pr650.getLum()
     [nm,s] = pr650.getSpectrum()
     spec[i,2,:] = s
     # measure blue spectrum
     ledSer.write('[m,0,0,1,0,0,1]\n');
     time.sleep(0.1)
     pr650.measure()
-    lum[i] = pr650.getLum()
+    lum[i,3] = pr650.getLum()
     [nm,s] = pr650.getSpectrum()
     spec[i,3,:] = s
     # measure white spectrum
     ledSer.write('[m,1,1,1,1,1,1]\n');
     time.sleep(0.1)
     pr650.measure()
-    lum[i] = pr650.getLum()
+    lum[i,4] = pr650.getLum()
     [nm,s] = pr650.getSpectrum()
     spec[i,4,:] = s
 
 pickle.dump(spec,open('tricolorLed.spec','w'))
 pickle.dump(nm,open('tricolorLed.nm','w'))
 pickle.dump(lum,open('tricolorLed.lum','w'))
+
+spec = pickle.load(open('tricolorLed.spec','r'))
+nm = pickle.load(open('tricolorLed.nm','r'))
+lum = pickle.load(open('tricolorLed.lum','r'))
+
+from pylab import *
+from numpy import *
+specMn = mean(spec,0)
+specSd = std(spec,0)
+figure
+errorbar(nm,specMn[0,:],specSd[0,:],color='k',capsize=0)
+errorbar(nm,specMn[1,:],specSd[1,:],color='r',capsize=0)
+errorbar(nm,specMn[2,:],specSd[2,:],color='g',capsize=0)
+errorbar(nm,specMn[3,:],specSd[3,:],color='b',capsize=0)
+errorbar(nm,specMn[4,:],specSd[4,:],color='y',capsize=0)
+#'r',nm,specMn[2,:],'g',nm,specMn[3,:],'b',nm,specMn[4,:],'y')
+xlabel('Wavelength (nm)')
+ylabel('Power (watts/sr/m^2/nm)')
+title('Spectral Calibration')
+grid(True)
+show()
+
+cones = numpy.fromfile('stockman4.txt',sep=' ')
+cones = cones.reshape(101,4)
+figure
+plot(cones[:,0],cones[:,1],'r',cones[:,0],cones[:,2],'g',cones[:,0],cones[:,3],'b')
+#'r',nm,specMn[2,:],'g',nm,specMn[3,:],'b',nm,specMn[4,:],'y')
+xlabel('Wavelength (nm)')
+ylabel('Relative absorption')
+title('Cone Fundamentals')
+show()
+
+rgb2lms = transpose(cones[:,1:3])*transpose(spectMn[1:3,:])
+lms2rgb = linalg.inv(rgb2lms)
+rgb2lms = around(rgb2lms/rgb2lms.max()*32767)
+
+stimLMS = array([0,0,1])
+
+lmsBack = dot(rgb2lms,(array([0.5,0.5,0.5])))
+# Scale stimulus LMS by the background LMSscaledStimLMS = stimLMS * lmsBack# Determine the stimulus RGB direction stimRGBdir = dot(lms2rgb,scaledStimLMS)stimRGBdir = stimRGBdir/abs(stimRGBdir/stimRGBdir).max()
+
+ledSer = serial.Serial('/dev/ttyUSB0', 57600, timeout=1)
+ledSer.write("[w,0,3,0,0.338,-0.2740,0.5209,0,0,0][p]")
+ledSer.write("[e,20,.2][w,0,10,0,0.338,-0.2740,0.5209,-0.338,0.2740,-0.5209][p]")
 
 # measure gamma
 nRepeats = 10;
